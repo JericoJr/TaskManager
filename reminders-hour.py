f442f834022df6871c04f85c7cadce0e851e7ae9
JericoJr <jericojrestrella@gmail.com>
2025-08-27 23:38:19 -0400

    my way

--- reminders-hour.py
+++ reminders-hour.py
@@ -42,15 +42,16 @@ def task_reminder_hour():
                 else:
                     task_deadline_local = task.deadline
 
-                current_time = datetime.now(user_tz)
+                # current_time = datetime.now(user_tz)
                 print(f"task_deadline: {task_deadline_local}; current_time: {datetime.now(timezone.utc).astimezone(user_tz)}")
                 # Now check if due within the next hour
-                # time_hour_left = task_deadline_local.hour - datetime.now(user_tz).hour
-                # time_min_left = task_deadline_local.minute - datetime.now(user_tz).minute
+                time_hour_left = task_deadline_local.hour - datetime.now(user_tz).hour
+                time_min_left = task_deadline_local.minute - datetime.now(user_tz).minute
 
-                # time_left_total_minutes = (time_hour_left * 60) + time_min_left
-                time_left = task_deadline_local - current_time
-                time_left_total_minutes = int(time_left.total_seconds() / 60)
+                time_left_total_minutes = (time_hour_left * 60) + time_min_left
+
+                # time_left = task_deadline_local - current_time
+                # time_left_total_minutes = int(time_left.total_seconds() / 60)
                 print(f"Time left: {time_left_total_minutes}")
                 # If time_left is <= 1 hour or between 0 to 60 minutes then send email
                 if 0 <= time_left_total_minutes<= 60:
